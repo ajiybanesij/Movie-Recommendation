7a76571830c0c472a3c36f4483d93cff97180184
Alperen Bozkurt <mail@alperenbozkurt.net>
2021-06-05 19:20:35 +0300

    Show Yapıldı Show

--- main.rb
+++ main.rb
@@ -1,50 +1,41 @@
-file = File.open("all_ratings.txt")
+file = File.open('all_ratings.txt')
 file_data = file.read
 
 films = {}
-i = 0
-file_data.each_line do |line|
-  if i != 0
-    i = i + 1
-    data = line.split("\t")
-    data[3] = data[3].chomp!.split("|")
-    film = {
-      name: data[2],
-      imdb: data[1]
-    }
-    data[3].each { |item|
-      films[item] = [] unless films.key?(item)
-      film_array = films[item]
-      film_array.push(film)
-      films[item] = film_array
-    }
-    a = data
+
+file_data.each_line.with_index do |line, index|
+  next if index.zero?
+
+  _id, imdb, name, categories = line.split("\t")
+  categories = categories.chomp!.split('|')
+
+  film = {
+    name: name,
+    imdb: imdb
+  }
+
+  categories.each do |category|
+    films[category] = [] if films[category].nil?
+    films[category].push film
   end
-  i = i + 1
 end
 
-films.each do |key, value|
-  films[key].sort_by! { |k| k[:imdb] }.reverse!
+films.each do |category, _value|
+  films[category].sort_by! { |film| film[:imdb] }.reverse!
 end
-puts "Select Movie Topic"
-print films.keys
-puts ""
 
-index = 0
+puts 'Select Movie Topic'
+print films.keys.join(' - ')
+puts ''
+
 topic = gets.chomp
-loop do
 
-  if films.key?(topic)
-    puts films[topic][index][:name]
-    puts "Do you want watch ? (y/n)"
-    ans = gets.chomp
-    if ans == "y"
-      break
-    else
-      index = index + 1
-    end
-  end
+puts films[topic].count
 
+films[topic].each do |film|
+  puts film[:name]
+  puts 'Do you want watch ? (y/n)'
+  ans = gets.chomp
+
+  break if ans.downcase == 'y'
 end
-
-
